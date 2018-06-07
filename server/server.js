2136033e91795b8b7082260ca155d25e6045aec6
joaorsilva <joao.r.silva@gmail.com>
2018-06-07 18:57:50 -0300

    Add Patch route test

--- server/server.js
+++ server/server.js
@@ -1,8 +1,8 @@
 
+const _ = require('lodash');
 const {ObjectID} = require('mongodb');
-
-var express = require('express');
-var bodyParser = require('body-parser');
+const express = require('express');
+const bodyParser = require('body-parser');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require ('./models/todo');
@@ -55,6 +55,31 @@ app.get('/todos/:id', (req, res) => {
         }).catch( (e) => {
             res.status(400).send();
         });            
+    }
+});
+
+app.patch('/todos/:id', (req, res) => {
+    var id=req.params.id;
+    var body = _.pick(req.body,['text','completed']);
+    //console.log('Received id:', id);
+    if(!ObjectID.isValid(id)) {
+        res.status(404).send();
+    } else {
+        if(_.isBoolean(body.completed) && body.completed) {
+            body.completedAt = new Date();
+        } else {
+            body.completed = false;
+            body.completedAt = null;
+        }
+        Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then( (todo) => {
+            if(!todo) {
+                return res.status(404).send();
+            }
+            res.send({todo});
+        }).catch(  (e) => {
+            console.log(e);
+            res.status(400).send();
+        });
     }
 });
 
