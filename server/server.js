875417fb0f5e7444608985e141a70f61964962b9
joaorsilva <joao.r.silva@gmail.com>
2018-06-08 10:42:07 -0300

    Add post users rout and token method

--- server/server.js
+++ server/server.js
@@ -103,6 +103,21 @@ app.delete('/todos/:id', (req, res) => {
     }
 });
 
+app.post('/users', (req, res) => {
+    var user = new User(
+        _.pick(req.body,['email','password'])
+    );
+
+    user.save().then( () => {
+        return user.generateAuthToken();
+    }).then( (token) => {
+        res.header('x-auth',token).send(user);
+    }).catch( (e) => {
+        console.log('ERROR:',e);
+        res.status(400).send(e);
+    });
+});
+
 app.listen(3000, () => {
     console.log(`Started at port ${port}`);
 });
